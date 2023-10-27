0177f728efb37e6afed43018f3bcbb7d675a7984
Minjee Song <smjteresa95@gmail.com>
2023-10-27 15:08:09 +0900

    db connection util added

---
+++ src/database/db_util.py
@@ -0,0 +1,11 @@
+import pymysql
+from src.config.db_config import DB_CONFIG
+
+def get_db_connection():
+    return pymysql.connect(
+        host=DB_CONFIG['host'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        database=DB_CONFIG['database'],
+        charset='utf8'
+    )
