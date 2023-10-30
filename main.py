1ddc44c45f550c7f8f0333421e9378916172b6c2
Minjee Song <smjteresa95@gmail.com>
2023-10-31 02:59:07 +0900

    save nutrition info to the database, then calculate and fill product filter table according to our nutrition criteria

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src.nutridata.parse_data import insert_sql_from_json
+from src.productfilter.save_nuntri_filter import SaveNutriFilter as nf
 
 if __name__ == '__main__':
 
     #DB에 기준일 이후의 공공데이터 저장 
     insert_sql_from_json('2023-08-09')
+
+    #product filter
+    filter = nf()
+    filter.fill_filter_column()
+
+
     
