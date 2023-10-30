305c8e64381dcb205284ac30d10f38b42f31330e
Minjee Song <smjteresa95@gmail.com>
2023-10-31 02:57:08 +0900

    database init file added, query function file added

--- src/database/query.py
+++ src/database/query.py
@@ -3,17 +3,11 @@ create_product_table_query = """
     CREATE TABLE IF NOT EXISTS product (
         product_id BIGINT NOT NULL UNIQUE PRIMARY KEY, 
         product_name VARCHAR(255) NOT NULL,
-        mfg_company VARCHAR(255),
-        category VARCHAR(255),
+        brand VARCHAR(255),
+        category_name VARCHAR(255),
         large_category VARCHAR(255),
         med_category VARCHAR(255),
         small_category VARCHAR(255),
-        site VARCHAR(255),
-        image TEXT,
-        product_link TEXT,
-        sales_price INT,
-        discount_rate DECIMAL(5,2),
-        discounted_price INT,
         total_serving VARCHAR(255),
         serving_size VARCHAR(255),
         kcal DECIMAL(10,2),
@@ -33,8 +27,40 @@ create_product_table_query = """
 # 식품코드를 unique key로 사용하여 공공데이터값 중복되지 않게 insert. 
 update_product_from_public_data_query = """
     INSERT INTO product
-    (product_id, product_name, mfg_company, category, large_category, med_category, small_category, total_serving, serving_size, kcal, carbohydrate, protein, fat, sodium, cholesterol, saturated_fat, trans_fat, sugar)
+    (product_id, product_name, brand, category_name, large_category, med_category, small_category, total_serving, serving_size, kcal, carbohydrate, protein, fat, sodium, cholesterol, saturated_fat, trans_fat, sugar)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     ON DUPLICATE KEY UPDATE
     product_id = VALUES(product_id)
     """
+
+#ProductFilter table 
+create_filter_table_query = """
+    CREATE TABLE IF NOT EXISTS product_filter(
+        product_id BIGINT NOT NULL,
+        category_name VARCHAR(255),
+        is_low_calorie BOOLEAN NOT NULL DEFAULT 0,
+        is_high_calorie BOOLEAN NOT NULL DEFAULT 0,
+        is_sugar_free BOOLEAN NOT NULL DEFAULT 0,
+        is_low_sugar BOOLEAN NOT NULL DEFAULT 0,
+        is_low_carb BOOLEAN NOT NULL DEFAULT 0,
+        is_high_carb BOOLEAN NOT NULL DEFAULT 0,
+        is_keto BOOLEAN NOT NULL DEFAULT 0,
+        is_low_trans_fat BOOLEAN NOT NULL DEFAULT 0,
+        is_low_protein BOOLEAN NOT NULL DEFAULT 0,
+        is_high_protein BOOLEAN NOT NULL DEFAULT 0,
+        is_low_sodium BOOLEAN NOT NULL DEFAULT 0,
+        is_low_cholesterol BOOLEAN NOT NULL DEFAULT 0,
+        is_low_saturated_fat BOOLEAN NOT NULL DEFAULT 0,
+        is_low_fat BOOLEAN NOT NULL DEFAULT 0,
+        is_high_fat BOOLEAN NOT NULL DEFAULT 0,
+        is_green BOOLEAN NOT NULL DEFAULT 0
+    )
+"""
+
+update_filter_query = """
+    INSERT INTO product_filter
+    (product_id, category_name, is_low_calorie, is_high_calorie, is_sugar_free, is_low_sugar, is_low_carb, is_high_carb, is_keto, is_low_trans_fat, is_high_protein, is_low_sodium, is_low_cholesterol, is_low_saturated_fat, is_low_fat, is_high_fat, is_green)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+    product_id = VALUES(product_id)
+"""
