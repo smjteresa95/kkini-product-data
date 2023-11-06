d02fa5d7ba45417e649bb9bde3e4bf122759f673
Minjee Song <smjteresa95@gmail.com>
2023-11-06 12:17:15 +0900

    column added to product table and save data

--- src/nutridata/get_nutri_data.py
+++ src/nutridata/get_nutri_data.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 import re #정규표현식을 쓰기 위한 Lib
 from src.config.db_config import DATA_PATH
 
@@ -15,14 +16,21 @@ class GetNutriData:
     @staticmethod
     def clean_code(value):
         #정규표현식 이용해서 product code 앞 P와 -삭제하고 int형으로 변환
-        return int(re.sub('[P-]','',value))
+        return int(re.sub('[P-]','',value)) 
+
+
+    @staticmethod
+    #상품명 앞 카테고리 붙는거 삭제
+    def clean_name(value):
+        #_를 기준으로 나누고, 맨 앞 요소(카테고리명) 삭제, 문자열로 변환
+        return ''.join(value.split('_')[1:])
     
 
     #데이터에 들어있는 문자 제거.
     @staticmethod
-    def subtract_g(value):
-        #단위g을 삭제
-        return value.replace('g','').strip()
+    def subtract_unit(value):
+        #단위g, ml을 삭제
+        return re.sub(r'\s*(g|ml)\b','', value.strip())
 
 
     #우리가 지정한 카테고리 값 넣어주기
@@ -38,3 +46,20 @@ class GetNutriData:
         for category, codes in categories.items():
             if code in codes:
                 return category
+            
+            
+    #문자열을 날짜 객체로 변경 strptime
+    @staticmethod
+    def convert_to_date(value):
+        return datetime.strptime(value, '%Y-%m-%d').date()
+
+
+    # 유통업체를 brand column에 넣되, 유통업체가 '해당없음'이면 '제조사명'에 해당하는 값 넣기. 둘 다 없으면 '정보없음' 넣어주기
+    @staticmethod
+    def get_brand(data):
+        if data.get('유통업체명') and data['제조사명'] != '해당없음':  
+            return data['유통업체명']
+        elif data.get('제조사명'):
+            return data['제조사명']
+        else:
+            return '정보없음'
--- src/nutridata/parse_data.py
+++ src/nutridata/parse_data.py
@@ -1,70 +1,83 @@
-#json파일에서 필요한 데이터들을 DB에 저장
+#json파일에서 필요한 데이터들을 DB의 product 테이블에 저장
 import pymysql
-from datetime import datetime
-import re #정규표현식을 쓰기 위한 Lib
 
 from src.database.db_util import get_db_connection
 from src.database.query import update_product_from_public_data_query, create_product_table_query
 
 from src.nutridata.get_nutri_data import GetNutriData as nd
+from src.productfilter.cal_nutri_filter import CalNutriFilter as nf
 
+class SaveProduct:
 
-#상품명 앞 카테고리 붙는거 삭제
-def clean_data(value):
-    #_를 기준으로 나누고, 맨 앞 요소(카테고리명) 삭제, 문자열로 변환
-    return ''.join(value.split('_')[1:])
+    #cutoff_date 이후의 데이터만 DB에 저장. 
+    def insert_sql_from_json(cutoff_date):
 
+        #공공데이터 가지고 오기
+        data_list = nd.fetch_json_data_from_file()
 
-#문자열을 날짜 객체로 변경 strptime
-def convert_to_date(value):
-    return datetime.strptime(value, '%Y-%m-%d').date()
+        #데이터 기준 일자가 특정일 이후인 데이터들만 가지고 오게하기.
+        #식품코드가 동일하면 update, 다르면 insert
+        cutoff_date = nd.convert_to_date(cutoff_date)
 
-# 유통업체를 brand column에 넣되, 유통업체가 '해당없음'이면 '제조사명'에 해당하는 값 넣기. 둘 다 없으면 '정보없음' 넣어주기
-def get_brand(data):
-    if data.get('유통업체명') and data['제조사명'] != '해당없음':  
-        return data['유통업체명']
-    elif data.get('제조사명'):
-        return data['제조사명']
-    else:
-        return '정보없음'
+        #dictionary에서 필요한 필드들만 빼서 tuple로 변환. for bulk update.
+        product_data = []
 
+        for data in data_list:
 
-#cutoff_date 이후의 데이터만 DB에 저장. 
-def insert_sql_from_json(cutoff_date):
-
-    #Mysql연결
-    conn = get_db_connection()
-
-    #sql 쿼리 실행해주고 결과 반환 해 줄 커서객체 생성
-    cur = conn.cursor()
-
-    #공공데이터 가지고 오기
-    data_list = nd.fetch_json_data_from_file()
-
-    #데이터 기준 일자가 특정일 이후인 데이터들만 가지고 오게하기.
-    #식품코드가 동일하면 update, 다르면 insert
-    cutoff_date = convert_to_date(cutoff_date)
-
-    #dictionary에서 필요한 필드들만 빼서 tuple로 변환. for bulk update.
-    product_data = [(nd.clean_code(data['식품코드']), clean_data(data['식품명']), get_brand(data), nd.set_category(data), data['식품대분류명'], data['식품중분류명'], data['식품소분류명'], 
-                        nd.subtract_g(data['식품중량']), nd.subtract_g(data['1회 섭취참고량']), data['에너지(kcal)'], 
-                        data['탄수화물(g)'], data['단백질(g)'], data['지방(g)'], data['나트륨(mg)'], 
-                        data['콜레스테롤(mg)'], data['포화지방산(g)'], data['트랜스지방산(g)'], data['당류(g)']) 
-                    for data in data_list if convert_to_date(data['데이터기준일자']) >= cutoff_date]
+            category = nd.set_category(data)
+            kcal = data['에너지(kcal)']
+            carb =  data['탄수화물(g)']
+            protein = data['단백질(g)']
+            fat = data['지방(g)']
+            sodium = data['나트륨(mg)']
+            cholesterol = data['콜레스테롤(mg)']
+            sat_fat = data['포화지방산(g)']
+            trans_fat = data['트랜스지방산(g)']
+            sugar = data['당류(g)']
     
-    try:
-        cur.execute(create_product_table_query)
-        cur.executemany(update_product_from_public_data_query, product_data)
-        conn.commit()
-        print("Successfully inserted records to database")
+            #is_green
+            cal_nutri_filter_instance = nf(
+                category = category,
+                kcal = kcal,
+                carb = carb,
+                protein = protein,
+                fat = fat,
+                sodium = sodium,
+                cholesterol = cholesterol,
+                sat_fat = sat_fat,
+                trans_fat = trans_fat,
+                sugar = sugar
+            )
 
-    except pymysql.Error as error:
-        print("Failed to insert records to database: {}".format(error))
+            is_green = cal_nutri_filter_instance.get_is_green()
 
-    finally:
-        conn.close()
-        print("MySQL connection is closed")
+            nut_score = cal_nutri_filter_instance.get_nut_score()
+
+            if nd.convert_to_date(data['데이터기준일자']) >= cutoff_date:
+                product_data.append([nd.clean_code(data['식품코드']), nd.clean_name(data['식품명']), nd.get_brand(data), category, data['식품대분류명'], data['식품중분류명'], data['식품소분류명'], 
+                                    data['식품중량'], nd.subtract_unit(data['영양성분함량기준량']), data['1회 섭취참고량'], kcal, 
+                                   carb, protein, fat, sodium, cholesterol, sat_fat, trans_fat, sugar, is_green, nut_score])
+                                
+        
+        #Mysql연결
+        conn = get_db_connection()
+
+        #sql 쿼리 실행해주고 결과 반환 해 줄 커서객체 생성
+        cur = conn.cursor()
+
+        try:
+            cur.execute(create_product_table_query)
+            cur.executemany(update_product_from_public_data_query, product_data)
+            conn.commit()
+            print("Successfully inserted records to database")
+
+        except pymysql.Error as error:
+            print("Failed to insert records to database: {}".format(error))
+
+        finally:
+            conn.close()
+            print("MySQL connection is closed")
 
 
-            
+                
 
