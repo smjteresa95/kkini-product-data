ef2b60c3aa181161cdffdcf2831564a147361e3d
Minjee Song <smjteresa95@gmail.com>
2023-11-13 15:14:05 +0900

    test file for saving image to the object storage

---
+++ main_s3.py
@@ -0,0 +1,16 @@
+from src.s3.image_saver import ImageSaver
+
+image_url = 'https://shopping-phinf.pstatic.net/main_5716183/5716183724.20210713112326.jpg?type=f640'
+#same as product_name
+file_name = '팔도비빔면'
+
+#크롤링 한 이미지 url로 객체생성
+image_content = ImageSaver.download_image(image_url)
+
+#객체를 object storage 에 저장
+if image_content:
+    ImageSaver.upload_to_s3(file_name, image_content)
+    print('image successfully updated to the object storage')
+
+#url 얻어오기
+print(ImageSaver.get_public_url(file_name))
