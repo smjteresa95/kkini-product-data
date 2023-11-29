cf9ef443188702ab09e3ff0537bcc4affe6d67e8
Minjee Song <smjteresa95@gmail.com>
2023-11-30 04:31:08 +0900

    main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.product.parse_data import SaveProduct as sp
 from src.productfilter.save_nutri_filter import SaveNutriFilter as nf
 from src.product.save_product_image import SaveProductImage as gi
+from src.webscraping.webscraper import WebScraper
 
 if __name__ == '__main__':
 
@@ -11,8 +12,11 @@ if __name__ == '__main__':
     # filter = nf()
     # filter.fill_filter_column()
 
+    # 웹 스크래핑 해서 product_info 테이블 채우기
+    WebScraper.get_data_from_site()
+
     #네이버에서 크롤링한 이미지 S3에 저장 후 DB에 public url 저장
-    gi.get_image_upload_to_s3()
+    # gi.get_image_upload_to_s3()
 
 
 
